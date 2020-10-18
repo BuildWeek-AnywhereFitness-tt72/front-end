51100ad82aa97934f31812e9c887d4454ba9a13b
chaz-kiker <chaz-kiker@lambdastudents.com>
2020-10-18 19:38:49 -0400

    resolve ESLint errors

--- src/Pages/Dash/Dash.js
+++ src/Pages/Dash/Dash.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 const LandingPage = styled.div`
@@ -48,7 +48,6 @@ const LandingPage = styled.div`
 		margin-top: 0;
 		height: 560px;
 		background-image: url("https://s3-alpha-sig.figma.com/img/9adf/3259/6e5496fba2de53fa2ad308e1607bae4b?Expires=1603670400&Signature=aXc3ptln6GHEwJfJhkE3VvPI0lk772EyadQgWoe0sqIqNtc5HrkUBNVVNWbNduvI5EN1pM~23vtZIXQDGtJqrXVTR1eZsljoCGCCUqHtETHPvyp5Nq5czwym3CLL-I0GXx1m5aYGbh-oY-nTeTrGaQIiaFbOBhLdQjr9BBiWQRIIujOi87yW6C81ZPfxJ1AvkwrAzWcs50aaY3wojabWg6JiMwfRl24~ryCblh5vGkDrAsrAYQp5RJTZClH~V2LyMyScK6o8bkHZxLrIpNnWfH8cETjag96V1qV2itgWsgo-FW940~Fjn~avYGJODdv5Yihipv-tg1f7-CdtACY7Cg__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA");
-		/* background-image: url("./activebody2"); */
 		/* background: rgba(76, 175, 80, 0.5) */
 		background-repeat: no-repeat;
 		max-width: 100%;
