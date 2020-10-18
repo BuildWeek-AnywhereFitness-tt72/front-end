6e0133ce5d30b7fc70af534d080aacf2f3ad36a3
Joe_Panetta <jlpanetta1681@icloud.com>
2020-10-18 16:07:53 -0400

    deleted register files so app would load

--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import styles from "./Register.module.css";
-import { Link } from "react-router-dom";
+// import React from 'react';
+// import styles from "./Register.module.css";
+// import { Link } from "react-router-dom";
 
-const Register = () => <h1 className={styles.sample}>Registration Page </h1>;
+// const Register = () => <h1 className={styles.sample}>Registration Page </h1>;
 
-export default Register;
+// export default Register;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+// import App from './App';
 import Routes from './Routes';
 import './index.css';
-import "bootstrap/dist/css/bootstrap.css";
-
+import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(<Routes />, document.getElementById('root'));
