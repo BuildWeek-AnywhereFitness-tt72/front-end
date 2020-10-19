3a97389e60114446466cc85dbe934966aa3a9163
chaz-kiker <chaz-kiker@lambdastudents.com>
2020-10-19 12:39:10 -0400

    fix App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import React from 'react'
 
 import './App.css'
 
-const App = (props) => props.children;
+const App = (props) => {
+	return(
+		<div>
+			{props.children}
+		</div>
+	);
+};
 
 export default App;
