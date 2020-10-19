018762cfbb6bb2717899153c7e349c404abca851
chaz-kiker <chaz-kiker@lambdastudents.com>
2020-10-19 03:41:19 -0400

    refactor Link to StyledLink

--- src/Pages/Dash/Dash.js
+++ src/Pages/Dash/Dash.js
@@ -48,61 +48,35 @@ const LandingPage = styled.div`
 		margin-top: 0;
 		height: 560px;
 		background-image: url("https://s3-alpha-sig.figma.com/img/9adf/3259/6e5496fba2de53fa2ad308e1607bae4b?Expires=1603670400&Signature=aXc3ptln6GHEwJfJhkE3VvPI0lk772EyadQgWoe0sqIqNtc5HrkUBNVVNWbNduvI5EN1pM~23vtZIXQDGtJqrXVTR1eZsljoCGCCUqHtETHPvyp5Nq5czwym3CLL-I0GXx1m5aYGbh-oY-nTeTrGaQIiaFbOBhLdQjr9BBiWQRIIujOi87yW6C81ZPfxJ1AvkwrAzWcs50aaY3wojabWg6JiMwfRl24~ryCblh5vGkDrAsrAYQp5RJTZClH~V2LyMyScK6o8bkHZxLrIpNnWfH8cETjag96V1qV2itgWsgo-FW940~Fjn~avYGJODdv5Yihipv-tg1f7-CdtACY7Cg__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA");
-		/* background: rgba(76, 175, 80, 0.5) */
 		background-repeat: no-repeat;
 		max-width: 100%;
 		background-size: cover;
 		background-position: center;
-		/* opacity: 0.3; */
 		background-blend-mode: darken;
 		div.hero-text-container {
 			background: rgba(248, 248, 248, 0.3);
 			height: 100%;
 		}
-			/* div.hero-text {
-
-				position: relative;
-				width: 770px;
-				height: 290px;
-				left: 25px;
-				top: 180px;
-				h3 {
-					font-size: 8.6rem;
-					line-height: 10.1rem;
-					letter-spacing: 0.02em;
-					color: ${pr => pr.theme.charcoal};
-				}
-				p {
-					font-weight: 600;
-					font-size: 1.8rem;
-					line-height: 2.5rem;
-					display: flex;
-					align-items: center;
-					color: ${pr => pr.theme.charcoal};
-				}
-			}
-			div.not-active {
-				display: none;
-			} */
 	}
-	div.signup-button {
+	div.link-container {
 		display: flex;
 		flex-flow: row wrap;
 		justify-content: center;
 		align-items: center;
-		a {
-			display: inline-block;
-			width: 28rem;
-			border-radius: 5px;
-			text-align: center;
-			font-size: 3rem;
-			background-color: ${pr => pr.theme.primaryOrange};
-			color: ${pr => pr.theme.charcoal};
-			text-decoration: none;
-			padding: 2rem;
-			margin: 1rem;
-		}
 	}
+`;
+
+const StyledLink = styled(Link)`
+	display: inline-block;
+	width: 28rem;
+	border-radius: 5px;
+	text-align: center;
+	font-size: 3rem;
+	background-color: ${pr => pr.theme.primaryOrange};
+	color: ${pr => pr.theme.charcoal};
+	text-decoration: none;
+	padding: 2rem;
+	margin: 1rem;
 `;
 
 const HeroTextContainer = styled.div`
@@ -126,7 +100,6 @@ const HeroTextContainer = styled.div`
 		align-items: center;
 		color: ${pr => pr.theme.charcoal};
 	}
-			
 `;
 
 const HelpCollapse = styled.div`
@@ -134,7 +107,6 @@ const HelpCollapse = styled.div`
 	transition: all 0.4s ease-in-out;
 	height: 250px;
 	max-width: 100%;
-	/* transition: all 0.4s ease-in-out; */
 	h3 {
 		font-size: 8.6rem;
 		line-height: 10.1rem;
@@ -183,10 +155,8 @@ const Dash = () => {
 	const [isHelpOpen, setIsHelpOpen] = useState(false);
 
 	const handleAboutClick = () => {
-		// if (isBrowseOpen) {
 		setIsAboutOpen(!isAboutOpen);
 		setIsBrowseOpen(!isBrowseOpen);
-		// }
 	};
 
 	const handleHelpClick = () => {
@@ -206,14 +176,10 @@ const Dash = () => {
 				<h4>The world is your gym.</h4>
 				<h4>Welcome.</h4>
 			</div>
-
 			<HelpCollapse className="help-collapse" isHelpOpen={isHelpOpen}>
 				<h3>Register/Cancel classes</h3>
 				<p>Signing up for classes is easy. And you can cancel and reschedule classes with little to no cancellation fee.</p>
 			</HelpCollapse>
-
-
-
 			<div className="background-img">
 				<div className="hero-text-container">
 					<HeroTextContainer className="hero-text" isOpen={isBrowseOpen} >
@@ -226,23 +192,20 @@ const Dash = () => {
 					</HeroTextContainer>
 				</div>
 			</div>
-			<div className="signup-button">
-				<Link to="/Login">Login</Link>
+			<div className="link-container">
+				<StyledLink to="/Login">Login</StyledLink>
 			</div>
-
 
 			{/* PURELY FOR EASE OF ACCESS — NOT PART OF THE PAGE */}
 			<DeveloperNav className="for-developers">
 				<h4>JUST FOR US</h4>
-				<p>This is simply for ease of access; not part of the page!. I figured it would be easier to navigate through all our pages if we had all the links in one spot</p>
+				<p>This is simply for ease of access; not part of the page! I figured it would be easier to navigate through all our pages if we had all the links in one spot</p>
 				<nav>
 					<Link to="/Login">Login</Link>
 					<Link to="/Dash">Dash</Link>
 					<Link to="/class-search">Class Search</Link>
 				</nav>
 			</DeveloperNav>
-
-
 		</LandingPage>
 	);
 };
